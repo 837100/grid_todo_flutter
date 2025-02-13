05fcd58875e0c8fbb014ac856ee12f4067f61480
837100 <shtjdrud0830@naver.com>
2025-02-13 17:49:37 +0900

    할 일 목록 화면에 추가 버튼 기능 구현

--- lib/todo_grid_view.dart
+++ lib/todo_grid_view.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
-
-
 class TodoGridView extends StatelessWidget {
   const TodoGridView({super.key});
+
+  void _addTodo() {
+    // 할 일 추가 로직을 여기에 작성하세요.
+    debugPrint('Add Todo');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('할 일'),
+        title: const Text('할 일 목록'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addTodo,
+          )
+        ]
       ),
       body: Scrollbar(
         thickness: 10,
@@ -27,7 +36,6 @@ class TodoGridView extends StatelessWidget {
             return InkWell(
               onTap: () {
                 debugPrint('Item $index Tapped');
-                
               },
               child: Container(
                 color: Colors.blue[100 * (index % 9)],
